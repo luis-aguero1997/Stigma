83b7fe68fdce03a080795492bc6f76ca58ac0f0b
Luis Moises Vega Agüero <luis_moises1997@hotmail.com>
2018-10-09 04:00:27 -0500

    Se agregaron elementos a el apartado de administrador

--- Stigma_GestorRiesgos/src/Administrador/BD.java
+++ Stigma_GestorRiesgos/src/Administrador/BD.java
@@ -21,9 +21,10 @@ public class BD {
     static ResultSet Resultado;
     private Connection Conexion;
 
+    
     public boolean Conectar() {
         try {
-            Class.forName("com.mysql.jdbc.Driver").newInstance();
+           Class.forName("com.mysql.jdbc.Driver").newInstance();
             Conexion = DriverManager.getConnection(
                     "jdbc:mysql://localhost/riesgos", "root", "");
             if (Conexion != null) {
@@ -44,4 +45,86 @@ public class BD {
             System.err.println(e.getMessage());
         }
     }
+    
+    public boolean AgregarUsuario(Usuario mUsuario) {
+        Statement consulta;
+        try {
+            consulta = Conexion.createStatement();
+            consulta.execute("insert into  usuarios "
+                    + "(nombre,nombre_user,email,password,tipo_user) "
+                    + "values ('" + mUsuario.getNombre() + "'," + mUsuario.getNomUser() + "," + mUsuario.getEmail() + "," + mUsuario.getPassword() + "," + mUsuario.getTipoUser() + ");");
+            return true;
+        } catch (Exception e) {
+            //  e.printStackTrace();
+            return false;
+        }
+    }
+    
+    public ArrayList ConsultarProductos() {
+        ArrayList mListaProductos = new ArrayList();
+        Producto mProducto = null;
+        Statement consulta;
+        ResultSet resultado;
+
+        try {
+            consulta = Conexion.createStatement();
+            resultado = consulta.executeQuery("select * from producto;");
+
+            while (resultado.next()) {
+                mUsuario = new Usuario();
+
+                mProducto.setId_Producto(resultado.getInt("id_producto"));
+                mProducto.setNombre(resultado.getString("nombre_producto"));
+                mProducto.setPrecio(Float.parseFloat(resultado.getString("precio_producto")));
+                mProducto.setPrecio_venta(Float.parseFloat(resultado.getString("precio_venta")));
+                mProducto.setCantidad(Integer.parseInt(resultado.getString("cantidad")));
+
+                mListaProductos.add(mProducto);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return mListaProductos;
+    }
+    
+    public ArrayList ConsultaFiltro(String Criterio) {
+        Usuario mUsuario = null;
+        Statement consulta;
+        ResultSet resultado;
+        ArrayList Lista = new ArrayList();
+
+        try {
+            mUsuario = new Usuario();
+            consulta = Conexion.createStatement();
+            resultado = consulta.executeQuery("select * from usuarios where idusuario like '%" + Criterio + "%' "
+                    + "or nombre like '%" + Criterio + "%' or nombreuser like '%" + Criterio  + "%' or email like'%" + Criterio + "%';");
+            while (resultado.next()) {
+                mUsuario = new Usuario();
+                
+                mUsuario.setNombre(resultado.getString("nombre"));
+                mUsuario.setNomUser(resultado.getString("nombreuser"));
+                mUsuario.setEmail(resultado.getString("email"));
+                mUsuario.setTipoUser(resultado.getString("tipouser"));
+                Lista.add(mUsuario);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return Lista;
+    }
+    
+    public boolean EliminarUsuario(int ID) {
+        Statement consulta;
+        try {
+            consulta = Conexion.createStatement();
+            consulta.execute("delete from usuarios"
+                    + " where idusuario = " + ID + ";");
+            return true;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+
 }
